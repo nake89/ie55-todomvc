4fde11e9b80ffec8ff4339f2fe9f3f518df966c7
nake89 <nake89@gmail.com>
2024-12-25 22:24:37 +0200

    got xhr hijacking to work

--- ide/public/hiddeninput/dev.js
+++ ide/public/hiddeninput/dev.js
@@ -67,11 +67,109 @@ else {
 })();
 
 
+// var currentNetworkRequest = {};
+
+// var originalXMLHttpRequest = XMLHttpRequest;
+// XMLHttpRequest = function() { originalXMLHttpRequest };
+// XMLHttpRequest.prototype.open = function (method, url, isAsync) {
+// 	currentNetworkRequest.method = method;
+// 	currentNetworkRequest.url = url;
+
+// };
+
+// function hijack(object, funcName, override) {
+//     var original = object[funcName];
+//     object[funcName] = override(original);
+//     return original;
+// }
+// hijack(XMLHttpRequest, 'open', function(original) {
+//     return function(method, url, isAsync) {
+// 	console.log(method);
+// 	console.log(url);
+//         original.call(this, method, url, isAsync);
+//     };
+// });
+// hijack(XMLHttpRequest, 'setRequestHeader', function(original) {
+//     return function(key, value) {
+// 	console.log(value);
+//         original.call(this, key, value);
+//     };
+// });
+
 window.onload = function() {
+
+// function hijack(scope, original, before){
+// 		console.log("hijack");
+// 	// for (var i = 0; i < arguments.length; i++) {
+// 		// console.log(arguments[i]);
+// 	// }
+//   var copy = original;
+//   return function(arguments){
+// 		console.log("ARGUMENTS");
+// 		console.log(arguments.length);
+// 		console.log(arguments);
+// 		console.log("hijack copy contructor name");
+// 		console.log(copy.constructor.name);
+// 		console.log("hijack before contructor name");
+// 		console.log(before.constructor.name);
+// 		console.log("hijack scope constructor name");
+// 		console.log(scope.constructor.name);
+//     before.apply(scope, arguments);
+//     copy.apply(scope, arguments);
+//   };
+// }
+
+// XMLHttpRequest.prototype.open = hijack(this, XMLHttpRequest.prototype.open, function() {
+// 	console.log("hijack prototype");
+// 	// for (var i = 0; i < arguments.length; i++) {
+// 	// 	console.log(arguments[i]);
+// 	// }
+// });
+
+
+XMLHttpRequest.prototype.open = (function() {
+	var originalFn = XMLHttpRequest.prototype.open
+	return function() {
+		console.log("arguments")
+		console.log(arguments.length)
+		console.log(arguments[0])
+		return originalFn.apply(this, arguments);
+	}
+})()
+
 	var obi = {name: "Obi-Wan Kenobi",
 	occupation: "Jedi"}
 	console.log(obi);
 	console.log("hello");
 	console.log("hello2");
 	console.log("hello3");
+
+
+var http = new XMLHttpRequest();
+console.log("Constructor name");
+console.log(http.constructor.name);
+
+		console.log("xml");
+var url = 'https://httpbin.org/get';
+
+http.open('GET', url, true);
+		console.log("open");
+
+//Send the proper header information along with the request
+http.setRequestHeader('Content-type', 'application/json');
+		console.log("header");
+
+http.onreadystatechange = function() {//Call a function when the state changes.
+		console.log("orig statechange");
+    if(http.readyState == 4 && http.status == 200) {
+        console.log(http.responseText);
+    } else {
+        console.log("httperror");
+        console.log(http.status);
+    }
+}
+		console.log("statechange");
+http.send();
+		console.log("send");
+
 }
